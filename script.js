a380fa1f549314ddfd1789f14b725b2ef74bdb34
NazimS <nazim.shixkerimov@gmail.com>
2023-03-08 12:48:02 +0400

    burger-menu hover effects ended, navbar position fixed added

--- script.js
+++ script.js
@@ -2,17 +2,21 @@
 // navbar fixed, scroll top begin
 window.onscroll = function (){myFunction()} 
 
-const topnav = document.querySelector(".nav-menu")
+const topnav = document.querySelector(".nav-menu");
 let sticky = topnav.offsetTop;
 
 function myFunction() {
 
-  if (window.pageYOffset >= sticky) {
-    topnav.classList.add("sticky");
-    document.querySelector(".connect-chevron").style.display = "block";
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+
+      topnav.classList.add("sticky");
+      document.querySelector(".connect-chevron").style.display = "block";
+
   } else {
-    topnav.classList.remove("sticky");
-    document.querySelector(".connect-chevron").style.display = "none";
+
+      topnav.classList.remove("sticky");
+      document.querySelector(".connect-chevron").style.display = "none";
+      
   }
 
 }
