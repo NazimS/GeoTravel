8a6b89a1b649940c651b55ae2c23aa304fe6ca20
NazimS <nazim.shixkerimov@gmail.com>
2023-03-10 23:11:37 +0400

    swiper edited

--- script.js
+++ script.js
@@ -21,6 +21,41 @@ function myFunction() {
 
 }
 
-
-
 // navbar fixed, scroll top end
+
+
+// slider begin
+
+$(document).ready(function () {
+  /// manshet slider ///
+  var galleryThumbs = new Swiper('div.manshet_appartment .gallery-thumbs', {
+    spaceBetween: 16,
+    slidesPerView: 4,
+    loop: true,
+    allowTouchMove: true,
+    freeMode: false,
+    loopedSlides: 4, //looped slides should be the same
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    navigation: {
+      nextEl: 'div.manshet_appartment .swiper-button-next',
+      prevEl: 'div.manshet_appartment .swiper-button-prev',
+    },
+    autoplay: {
+      delay: 3000,
+      slideSpeed: 3000,
+      disableOnInteraction: false,
+    },
+  });
+  var galleryTop = new Swiper('div.manshet_appartment .gallery-top', {
+    spaceBetween: 16,
+    loop: true,
+    loopedSlides: 4, //looped slides should be the same
+    thumbs: {
+      swiper: galleryThumbs,
+    },
+    effect: 'fade',
+  });
+});
+
+// slider end
